e203da042fae1bddfe9a54a79ba9d0599422e122
Ricardo Gomes <sleiph@gmail.com>
2020-10-02 21:22:37 -0300

    adicionando o controle do jogador

---
+++ Assets/Scripts/movimentoJogador.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentoJogador : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            if (position.z < 7) {
+                position.z++;
+                this.transform.position = position;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            if (position.z > 0) {
+                position.z--;
+                this.transform.position = position;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            if (position.x < 7) {
+                position.x++;
+                this.transform.position = position;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            if (position.x > 0) {
+                position.x--;
+                this.transform.position = position;
+            }
+        }
+    }
+}

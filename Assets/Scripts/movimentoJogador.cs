6f17085c04b67a62f43570f2e6dfd7af56ca5dec
Ricardo Gomes <sleiph@gmail.com>
2020-12-16 20:32:33 -0300

    automatizando o spawn de plataformas

--- Assets/Scripts/movimentoJogador.cs
+++ Assets/Scripts/movimentoJogador.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class movimentoJogador : MonoBehaviour
 {
+    // referencias
+    private plataformaSpawner platSpawner;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        platSpawner = transform.parent.GetComponent<plataformaSpawner>();
     }
 
     // Update is called once per frame
@@ -16,34 +19,38 @@ public class movimentoJogador : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             Vector3 position = this.transform.position;
-            if (position.z < 7) {
+            //if (position.z < 7) {
                 position.z++;
                 this.transform.position = position;
-            }
+            //}
+            platSpawner.SpawnTabuleiro();
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             Vector3 position = this.transform.position;
-            if (position.z > 0) {
+            //if (position.z > 0) {
                 position.z--;
                 this.transform.position = position;
-            }
+            //}
+            platSpawner.SpawnTabuleiro();
         }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Vector3 position = this.transform.position;
-            if (position.x < 7) {
+            //if (position.x < 7) {
                 position.x++;
                 this.transform.position = position;
-            }
+            //}
+            platSpawner.SpawnTabuleiro();
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Vector3 position = this.transform.position;
-            if (position.x > 0) {
+            //if (position.x > 0) {
                 position.x--;
                 this.transform.position = position;
-            }
+            //}
+            platSpawner.SpawnTabuleiro();
         }
     }
 }
---
+++ Assets/Scripts/plataformaSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformaSpawner : MonoBehaviour
+{
+    // referencias
+    public GameObject platPrefab;
+    private Vector3 jPos;
+    private Transform tabuleiro;
+
+    // funcoes
+    public void SpawnTabuleiro() {
+        tabuleiro = transform.GetChild(0);
+        foreach (Transform p in tabuleiro.transform)
+            GameObject.Destroy(p.gameObject);
+
+        jPos = transform.GetChild(1).position;
+        int jX = (int)jPos.x,
+            jZ = (int)jPos.z;
+        Quaternion rotacao = Quaternion.Euler(-90, 0, 0);
+
+        for (int i=jX-4; i<=jX+4; i++) {
+            for (int j=jZ-4; j<=jZ+4; j++) {
+                GameObject plataforma = Instantiate(platPrefab, new Vector3(i, 0, j), rotacao);
+                plataforma.transform.parent = tabuleiro;
+            }
+        }
+    } 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnTabuleiro();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

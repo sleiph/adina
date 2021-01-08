6d03de138a61ae12079ae65ef0bf6cc2e2fb33de
Ricardo Gomes <sleiph@gmail.com>
2021-01-08 19:49:21 -0300

    adicionando um sistema de selecao de plataformas

---
+++ Assets/Scripts/MovimentoMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoMouse : MonoBehaviour
+{
+    // variaveis
+
+    // funcoes
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit) {
+                if (hitInfo.transform.gameObject.tag == "plataforma") {
+                    hitInfo.transform.GetComponent<platControl>().isSelectado = true;
+                }
+            }
+        } 
+    }
+}
---
+++ Assets/Scripts/platControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platControl : MonoBehaviour
+{
+    // variaveis
+    public bool isSelectado = false;
+    bool m_isSelectado = false;
+
+    public Material[] selecMat;
+    public Material[] deselecMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSelectado != m_isSelectado) {
+            if (isSelectado)
+                GetComponent<MeshRenderer>().materials = selecMat;
+            else
+                GetComponent<MeshRenderer>().materials = deselecMat;
+            m_isSelectado = isSelectado;
+        }
+    }
+}

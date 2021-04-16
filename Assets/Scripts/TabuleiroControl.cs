56921df1ec674bfad4fdd5ad7c244d321db31de1
Ricardo Gomes <sleiph@gmail.com>
2021-04-15 22:04:44 -0300

    spawnando jogadores

--- Assets/Scripts/TabuleiroControl.cs
+++ Assets/Scripts/TabuleiroControl.cs
@@ -46,6 +46,7 @@ public class Jogador
     private float z;
     private int estado = 0;
     private int alcance;
+    private GameObject corpo;
     private Material sprite;
 
     // construtor
@@ -61,6 +62,31 @@ public class Jogador
     }
     public void setEstado(int e) {
         estado = e;
+    }
+
+    public Vector3 getPos()
+    {
+        return new Vector3(x, 0, z);
+    }
+    public void setPos(float xPos, float zPos) {
+        x = xPos;
+        z = zPos;
+    }
+
+    public float getX()
+    {
+        return x;
+    }
+    public float getZ()
+    {
+        return z;
+    }
+
+    public GameObject getCorpo() {
+        return corpo;
+    }
+    public void setCorpo(GameObject o) {
+        corpo = o;
     }
 
     public Material getMaterial() {
@@ -110,33 +136,57 @@ public class Inimigo : Jogador
 public class TabuleiroControl : MonoBehaviour
 {
     // variaveis
-    public GameObject plataforma;
+    public GameObject platPrefab;
+    public GameObject jogPrefab;
+    private int nAliados = 0;
+    private int nMigos = 0;
 
     Peca[,] tabuleiro = new Peca[10,10];
     Aliado[] amigos = new Aliado[5];
     Inimigo[] inimigos = new Inimigo[5];
 
-    Aliado protagonista = new Aliado(4, 4, 2);
-    Aliado amiga = new Aliado(8, 2, 2);
-
     // funcoes
-    void Start()
-    {
-        for (int i=0; i<10; i++) {
-            for (int j=0; j<10; j++) {
+    void tabuleiroSpawn(int tamanhoX, int tamanhoZ) {
+        for (int i=0; i<tamanhoX; i++) {
+            for (int j=0; j<tamanhoZ; j++) {
                 Peca peca = new Peca(i, j);
                 if (j%2!=0) {
                     peca.setPos(i-0.5f, j);
                 }
-                GameObject myPeca = Instantiate(plataforma, peca.getPos(), Quaternion.identity);
+                GameObject myPeca = Instantiate(platPrefab, peca.getPos(), Quaternion.identity);
                 myPeca.transform.parent = this.transform;
                 peca.setCorpo(myPeca);
                 tabuleiro[i, j] = peca;
             }
         }
+    }
+
+    void aliadoSpawn(float xPos, float zPos, int alcance) {
+        Aliado aliado = new Aliado(xPos, zPos, alcance);
+
+        GameObject myAliado = Instantiate(
+            jogPrefab,
+            aliado.getPos(),
+            Quaternion.identity
+        );
+
+        myAliado.transform.parent = tabuleiro[
+            (int)xPos, (int)zPos
+        ].getCorpo().transform;
+        aliado.setCorpo(myAliado);
+        amigos[nAliados] = aliado;
+
+        nAliados++;
+    }
+
+    void Start()
+    {
+        tabuleiroSpawn(10, 10);
+
         Debug.Log(tabuleiro[2, 7].getCorpo().transform.position);
 
-        
+        aliadoSpawn(4, 4, 2);
+        aliadoSpawn(8, 2, 3);
     }
 
     void Update()
